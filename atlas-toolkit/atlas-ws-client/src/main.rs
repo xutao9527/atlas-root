f672109a1bcb4c89c29336fb5f24bf1214efcdc0
xutao <xutao@gmail.com>
2025-12-30 11:19:34 +0800

    net

--- atlas-toolkit/atlas-ws-client/src/main.rs
+++ atlas-toolkit/atlas-ws-client/src/main.rs
@@ -7,28 +7,26 @@ use atlas_core::net::router::RouterMethod;
 
 #[tokio::main]
 async fn main() {
-    let url = "ws://127.0.0.1:8080/ws";
-    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    let ws_server_addr = "ws://127.0.0.1:8080/ws";
+
+    let (ws_stream, _) = connect_async(ws_server_addr).await.expect("Failed to connect");
 
     println!("Connected to WebSocket server");
     let (mut write, mut read) = ws_stream.split();
 
-    let req = Request {
+    // 发送一条文本消息
+    write.send(Message::Text("aa".into())).await.expect("send text failed");
+    // 发送二进制消息
+    let packet = Packet::Request(Request {
         id: 1,
         slot_index: 1,
         method: AuthMethod::SignIn.wire(),
         payload: vec![],
-    };
-    //self.pending.insert(&mut req, Box::new(callback)).await;
-    let packet = Packet::Request(req);
-
-    // 发送一条文本消息
-    write.send(Message::Text("aa".into())).await.expect("send text failed");
-    // 发送二进制消息
+    });
     let vec  = rmp_serde::to_vec(&packet).expect("serialize failed");
     let bytes = bytes::Bytes::from(vec);
+    write.send(Message::Binary(bytes)).await.expect("send binary failed");
 
-    write.send(Message::Binary(bytes)).await.expect("send text failed");
     while let Some(msg) = read.next().await {
         match msg {
             Ok(Message::Text(text)) => {
