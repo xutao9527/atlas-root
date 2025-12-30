d66091b4e77ad1d86cf65aed544d15e614ead9ea
xutao <xutao@gmail.com>
2025-12-30 17:29:10 +0800

    net

--- atlas-core/src/bin/server.rs
+++ atlas-core/src/bin/server.rs
@@ -1,9 +1,8 @@
 use atlas_core::net::packet::Response;
-use atlas_core::net::router::Router;
 use atlas_core::net::router::auth::AuthMethod;
+use atlas_core::net::router::Router;
 use atlas_core::net::server::AtlasNetServer;
 use futures::FutureExt;
-use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
