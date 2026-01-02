836af3c49c672dbb4c0b850381c1aa7ae4589ce2
xutao <xutao@gmail.com>
2026-01-02 22:09:22 +0800

    net

--- atlas-core/src/macros.rs
+++ atlas-core/src/macros.rs
@@ -1,51 +1,4 @@
 #[macro_export]
-macro_rules! atlas_module_method {
-    (
-        module $mod_name:ident {
-            module_id = $module_id:expr;
-            $(
-                $method_ty:ident = ($method_id:expr, $req_ty:ty, $resp_ty:ty, $fn_name:path)
-            ),* $(,)?
-        }
-    ) => {
-        pub mod $mod_name {
-            use super::*;
-            use bytes::Bytes;
-            use atlas_core::AtlasMethodSpec;
-            use atlas_core::net::rpc::packet_request::AtlasRawRequest;
-            use atlas_core::net::rpc::packet_response::AtlasRawResponse;
-            use atlas_core::net::rpc::router::handle;
-
-            $(
-                #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-                pub struct $method_ty;
-
-                impl ::atlas_core::AtlasMethodSpec for $method_ty {
-                    const MODULE_ID: ::atlas_core::AtlasModuleId = $module_id;
-                    const METHOD_ID: u16 = $method_id;
-                    type Request = $req_ty;
-                    type Response = $resp_ty;
-                }
-            )*
-
-            pub async fn dispatch(raw: AtlasRawRequest) -> AtlasRawResponse {
-                match raw.method {
-                    $(
-                        $method_ty::WIRE => handle::<$method_ty, _>(raw, $fn_name).await,
-                    )*
-                    _ => AtlasRawResponse {
-                        id: raw.id,
-                        slot_index: raw.slot_index,
-                        payload: Bytes::new(),
-                        error: Some("method not found".into()),
-                    },
-                }
-            }
-        }
-    };
-}
-
-#[macro_export]
 macro_rules! atlas_method {
     (
         module $mod_name:ident {
@@ -71,7 +24,6 @@ macro_rules! atlas_method {
         }
     };
 }
-
 
 #[macro_export]
 macro_rules! atlas_dispatch {
