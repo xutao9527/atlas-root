60233534e7f72321710ad70f154c7be08ceea476
xutao <xutao@gmail.com>
2026-01-07 19:15:46 +0800

    new

--- atlas-logic/atlas-auth/src/rpc/handler/account_handler.rs
+++ atlas-logic/atlas-auth/src/rpc/handler/account_handler.rs
@@ -10,28 +10,6 @@ use ulid::Ulid;
 
 pub async fn register(request: AtlasWireMessage<RegisterReq>) -> AtlasWireMessage<RegisterResp> {
     let register_req = request.payload;
-
-    let id = Ulid::new();
-
-    // 1️⃣ 官方标准：Crockford Base32（26 位，最常用）
-    println!("ULID string (base32): {}", id.to_string());
-
-    // 2️⃣ 原始字节（[u8; 16]）
-    let bytes = id.to_bytes();
-    println!("ULID bytes ([u8;16]): {:?}", bytes);
-
-
-
-    // 4️⃣ 时间戳部分（毫秒）
-    println!("ULID timestamp (ms): {}", id.timestamp_ms());
-
-    // 5️⃣ 从 bytes 还原 ULID
-    let id_from_bytes = Ulid::from_bytes(bytes);
-    println!("From bytes -> ULID: {}", id_from_bytes);
-
-
-
-
     let model = atlas_user::Model {
         id: Ulid::new().to_string(),
         user_type: Option::from(UserType::Normal),
@@ -51,7 +29,7 @@ pub async fn register(request: AtlasWireMessage<RegisterReq>) -> AtlasWireMessag
                 header: request.header.with_kind(ResponseOk),
                 payload: RegisterResp {
                     ok: true,
-                    message: None,
+                    message: Some("register success".into()),
                 }
             }
         }
@@ -61,7 +39,7 @@ pub async fn register(request: AtlasWireMessage<RegisterReq>) -> AtlasWireMessag
                 header: request.header.with_kind(ResponseErr),
                 payload: RegisterResp {
                     ok: false,
-                    message: None,
+                    message: Some(format!("register failed: {}", e)),
                 }
             }
         }
