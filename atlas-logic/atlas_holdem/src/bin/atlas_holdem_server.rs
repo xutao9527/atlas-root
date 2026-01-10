a66f7167372ef005e161bd3c285488e7b54b1139
xutao <xutao@gmail.com>
2026-01-10 14:55:30 +0800

    new

--- atlas-logic/atlas_holdem/src/bin/atlas_holdem_server.rs
+++ atlas-logic/atlas_holdem/src/bin/atlas_holdem_server.rs
@@ -35,7 +35,7 @@ async fn run_cmd(){
     handle_cmd("sit 2 2000".into()).await;
     handle_cmd("sit 3 2000".into()).await;
     handle_cmd("sit 4 500".into()).await;
-    handle_cmd("sit 5 2000".into()).await;
+    // handle_cmd("sit 5 2000".into()).await;
     // handle_cmd("sit 6 2000".into()).await;
     // handle_cmd("sit 7 1000".into()).await;
     // handle_cmd("sit 8 1200".into()).await;
--- atlas-logic/atlas_holdem/src/model/table.rs
+++ atlas-logic/atlas_holdem/src/model/table.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::process::Command;
 use ulid::Ulid;
 use crate::model::card::{Card, Deck};
 
@@ -449,10 +450,20 @@ impl Table {
     }
 }
 
+fn clear_screen() {
+    if cfg!(target_os = "windows") {
+        let _ = Command::new("cmd").arg("/c").arg("cls").status();
+    } else {
+        // 其他系统用 ANSI 转义码
+        print!("\x1B[2J\x1B[1;1H");
+    }
+}
+
 impl fmt::Display for Table {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // \x1B[2J: 清屏, \x1B[1;1H: 光标移动到 (1,1)
-        writeln!(f, "\x1B[2J\x1B[1;1H")?;
+        //writeln!(f, "\x1B[2J\x1B[1;1H")?;
+        clear_screen();
         writeln!(f, "\n{}", "=".repeat(80))?;
         writeln!(f, "TABLE ID: {} | HAND ID: {}", self.id, self.hand_id)?;
         writeln!(f, "STATE: {:?} | STREET: {:?}", self.state, self.street)?;
@@ -475,7 +486,7 @@ impl fmt::Display for Table {
                 Some(p) => {
                     let current_turn_mark = if self.current_turn == i { "*" } else { " " };
                     let last_raiser_mark = if self.last_raiser_pos == i { "R" } else { " " };
-                    let is_active_mark = if p.is_active { "✅" } else { "❌" };
+                    let is_active_mark = if p.is_active { "√" } else { "×" };
                     let is_all_in_mark = if p.is_all_in { " all-in " } else { "        " };
                     let dentity_mark = if self.dealer_pos == i {  "D" }
                     else if self.big_blind_pos == i {  "B" }
@@ -494,13 +505,13 @@ impl fmt::Display for Table {
                     // [*][R][S/B/D][i]  nickname balance Cards
                     writeln!(f,
                              " [{}][{}][{}]  [{}]: {} {} [{:>6}]   [{:>6}] [{:>6}]      |     Cards: {}",
-                             current_turn_mark, last_raiser_mark,dentity_mark, i,
-                             p.nickname, is_active_mark,is_all_in_mark,p.balance, p.street_bet,hole_cards_str
+                             current_turn_mark, last_raiser_mark, dentity_mark, i,
+                             p.nickname, is_active_mark, is_all_in_mark, p.balance, p.street_bet, hole_cards_str
                     )?;
                 }
                 None => {
                     writeln!(f,
-                             "            [{}] (Empty Seat)", i)?;
+                             "            [{}]  ( Empty )", i)?;
                 }
             }
         }
