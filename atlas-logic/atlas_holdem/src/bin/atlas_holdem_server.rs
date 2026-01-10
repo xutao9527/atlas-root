4edd93be3acfcba822fc5ae61880a1f40e23bb0d
xutao <xutao@gmail.com>
2026-01-10 19:41:47 +0800

    new

--- atlas-logic/atlas_holdem/src/bin/atlas_holdem_server.rs
+++ atlas-logic/atlas_holdem/src/bin/atlas_holdem_server.rs
@@ -30,17 +30,16 @@ async fn run_cmd(){
             }
         }
     });
-    handle_cmd("sit 0 2000".into()).await;
-    handle_cmd("sit 1 2000".into()).await;
-    handle_cmd("sit 2 2000".into()).await;
-    handle_cmd("sit 3 2000".into()).await;
-    handle_cmd("sit 4 1500".into()).await;
-    // handle_cmd("sit 5 2000".into()).await;
-    // handle_cmd("sit 6 2000".into()).await;
-    // handle_cmd("sit 7 1000".into()).await;
-    // handle_cmd("sit 8 1200".into()).await;
-    // handle_cmd("sit 9 2000".into()).await;
-    handle_cmd("start".into()).await;
+    tokio::spawn(async {
+        tokio::time::sleep(std::time::Duration::from_millis(300)).await;
+        handle_cmd("sit 0 2000".into()).await;
+        handle_cmd("sit 1 2000".into()).await;
+        handle_cmd("sit 2 2000".into()).await;
+        handle_cmd("sit 3 2000".into()).await;
+        handle_cmd("sit 4 1500".into()).await;
+        handle_cmd("start".into()).await;
+        handle_cmd("show".into()).await;
+    });
     while let Some(cmd) = cmd_rx.recv().await {
         if !handle_cmd(cmd).await {
             break
