d136b60e7535c5a1c482d31b65a37832e52cb1cd
xutao <xutao@gmail.com>
2026-01-27 18:25:17 +0800

    new

--- atlas-toolkit/atlas-codegen/src/model/tera_model.rs
+++ atlas-toolkit/atlas-codegen/src/model/tera_model.rs
@@ -5,3 +5,15 @@ pub struct TeraFieldCtx {
     pub name: String,
     pub ts_type: String,
 }
+
+#[derive(Serialize)]
+pub struct TsEnumVariant {
+    pub name: String,
+    pub payload: Option<String>,
+}
+
+#[derive(Serialize)]
+pub  struct TsEnumNumberVariant {
+    pub name: String,
+    pub value: String,
+}
