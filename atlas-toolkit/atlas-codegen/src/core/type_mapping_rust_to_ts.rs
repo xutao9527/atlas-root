8e44bc45a51bd9dbf95bb4eca6738d1508889c2e
xutao <xutao@gmail.com>
2026-01-27 17:06:33 +0800

    new

--- atlas-toolkit/atlas-codegen/src/core/type_mapping_rust_to_ts.rs
+++ atlas-toolkit/atlas-codegen/src/core/type_mapping_rust_to_ts.rs
@@ -8,11 +8,14 @@ pub fn rust_type_to_ts(ty: &Type) -> String {
             let ident = seg.ident.to_string();
             println!("{}", ident.as_str());
             match ident.as_str() {
+                // ===== 原始类型 =====
                 "String" | "str" => "string".to_string(),
                 "u8" | "u16" | "u32" | "u64" |
                 "i8" | "i16" | "i32" | "i64" |
+                "usize" |
                 "f32" | "f64" => "number".to_string(),
                 "bool" => "boolean".to_string(),
+                // ===== Option<T> =====
                 "Option" => {
                     // 递归解析 Option<T>
                     if let PathArguments::AngleBracketed(args) = &seg.arguments {
@@ -22,6 +25,7 @@ pub fn rust_type_to_ts(ty: &Type) -> String {
                     }
                     "any | null".to_string()
                 },
+                // ===== Vec<T> =====
                 "Vec" => {
                     // 递归解析 Vec<T>
                     if let PathArguments::AngleBracketed(args) = &seg.arguments {
@@ -31,7 +35,10 @@ pub fn rust_type_to_ts(ty: &Type) -> String {
                     }
                     "any[]".to_string()
                 },
-                _ => "any".to_string()
+                // ===== 👇 关键：自定义复合类型 =====
+                _ => {
+                    ident
+                }
             }
         }
         _ => "any".to_string(),
